eea2611c11341d28d695fd1d2b5531710114226e
d1laj <dimitri.lajou@ens-lyon.fr>
2016-03-29 15:18:11 +0200

    Separration du code

--- source/ProjetImage.cpp
+++ source/ProjetImage.cpp
@@ -1,54 +1,53 @@
-#include <stdio.h>      /* printf, scanf, puts, NULL */
-#include <stdlib.h>     /* srand, rand */
-#include <time.h>       /* time */
-#include <math.h>
-#include <iostream>
-#include <vector>
-#include <map>
-#include <string>
-#include <DGtal/base/Common.h>
-#include <DGtal/io/boards/Board2D.h>
-#include <DGtal/helpers/StdDefs.h>
-#include <DGtal/io/readers/PGMReader.h>
-#include "DGtal/images/imagesSetsUtils/SetFromImage.h"
-#include "DGtal/images/ImageContainerBySTLVector.h"
+#include "ProjetImage.hpp"
 
 using namespace std;
 
 int main(int argc, char** argv){
-	if (argc != 4){
-		fprintf(stderr, "Usage: ./ProjetImage directoryNameTrainSet directoryNameTestSet classes.csv\n");
-		exit(EXIT_FAILURE);
-	}
-
-	string directoryTrain = argv[1];
-
-	/* We get the different classes */
-  map<string, int> classes;
-
-	ifstream file ( argv[3] );
-  int i = 0;
-	string key;
-	while ( file.good() ){
-		   getline( file, key, ',' );
-       ++i;
-       if (key.size() >0 && key[0] == '\n'){
-         key.erase(0, 1);
-       }
-       if (key.size() > 0){
-         classes[key]= i;
-       }
-	}
-
-  printf("Classes :\n");
-  for(auto const &it : classes) {
-    cout << "classe: " << it.first << " | ind: " << it.second << "\n";
+  if (argc != 4){
+    fprintf(stderr, "Usage: ./ProjetImage directoryNameTrainSet directoryNameTestSet classes.csv\n");
+    exit(EXIT_FAILURE);
   }
 
-	/* Pour chaque fichier dans le dossier */
-		/* Feature extraction */
+  string directoryTrain = argv[1];
 
-	/* Learning */
+  /* We get the different classes */
+  map<string, int> classes;
+  get_classes(argv[3], classes);
 
+  int nbClasses = classes.size();
+
+
+
+  /* Pour chaque fichier dans le dossier */
+    /* Feature extraction */
+
+  /* Learning */
+
+
+}
+
+void get_classes(char* filename, map<string, int> & classes){
+  // Open the file
+  ifstream file ( filename );
+  int i = 0;
+  string key;
+
+  while ( file.good() ){
+    // Get a line
+    getline( file, key, ',' );
+    ++i;
+    // Add the class
+    if (key.size() > 0 ){
+      if (key[0] == '\n'){
+        key.erase(0, 1);
+      }
+      classes[key]= i;
+    }
+  }
+
+  fprintf(stderr, "Classes :\n");
+  for(auto const &it : classes) {
+    cerr << "classe: " << it.first << " | ind: " << it.second << "\n";
+  }
 
 }
---
+++ source/ProjetImage.hpp
@@ -0,0 +1,28 @@
+#include <stdio.h>      /* printf, scanf, puts, NULL */
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
+#include <math.h>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+#include <DGtal/base/Common.h>
+#include <DGtal/io/boards/Board2D.h>
+#include <DGtal/helpers/StdDefs.h>
+#include <DGtal/io/readers/PGMReader.h>
+/*
+#include "DGtal/images/imagesSetsUtils/SetFromImage.h"
+#include "DGtal/images/ImageContainerBySTLVector.h"
+*/
+
+using namespace std;
+
+#ifndef PROJET_IMAGE_HEADER
+#define PROJET_IMAGE_HEADER
+
+/* Look at the classes.csv file and store the classes with an index for each of them*/
+void get_classes(char* filename, map<string, int> & classes);
+
+/* Train set */
+
+#endif
