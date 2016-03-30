5da0467c00affa22913028fd693d68d334075c37
d1laj <dimitri.lajou@ens-lyon.fr>
2016-03-30 13:44:39 +0200

    bli

--- source/ProjetImage.hpp
+++ source/ProjetImage.hpp
@@ -1,26 +1,31 @@
-#include <stdio.h>      /* printf, scanf, puts, NULL */
-#include <stdlib.h>     /* srand, rand */
-#include <time.h>       /* time */
+#ifndef PROJET_IMAGE_HEADER
+#define PROJET_IMAGE_HEADER
+
+/* Standard C++ librairies  */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <math.h>
 #include <iostream>
 #include <vector>
 #include <map>
 #include <string>
+#include <fstream>
+#include <dirent.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+/* DGtal */
 #include <DGtal/base/Common.h>
 #include <DGtal/io/boards/Board2D.h>
 #include <DGtal/helpers/StdDefs.h>
 #include <DGtal/io/readers/PGMReader.h>
+#include <DGtal/images/imagesSetsUtils/SetFromImage.h>
+#include <DGtal/images/ImageContainerBySTLVector.h>
+/* Librairies linked to data learning */
 #include <armadillo>
 
-/*
-#include "DGtal/images/imagesSetsUtils/SetFromImage.h"
-#include "DGtal/images/ImageContainerBySTLVector.h"
-*/
-
 using namespace std;
-
-#ifndef PROJET_IMAGE_HEADER
-#define PROJET_IMAGE_HEADER
 
 /* Look at the classes.csv file and store the classes with an index for each of them*/
 void get_classes(char* filename, map<string, int> & classes);
