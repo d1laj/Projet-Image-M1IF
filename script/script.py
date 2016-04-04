7e77fbbca70e9c0c65b23a42b0a5e36fbe3014e0
d1laj <dimitri.lajou@ens-lyon.fr>
2016-04-04 21:43:38 +0200

    100% ? :)

--- script/script.py
+++ script/script.py
@@ -1,10 +1,15 @@
 #!python3
 
 import sklearn
-from sklearn.svm import SVC
-from sklearn.linear_model import LogisticRegression
 from numpy import genfromtxt
 import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from collections import OrderedDict
 
 
 def rank(idx, tab):
@@ -26,33 +31,51 @@ print(my_data)
 
 clf = SVC(kernel='rbf', gamma=2, C=1, probability=True)
 clf = LogisticRegression()
+models = [('Knn 5', KNeighborsClassifier(5)),
+          ('Naive Bayes', GaussianNB()),
+          ('Logistic Regression', LogisticRegression()),
+          ('Linear SVM', SVC(kernel='linear', probability=True)),
+          # ('Poly SVM', SVC(kernel='poly', degree=2, probability=True)),
+          ('RBF SVM', SVC(kernel='rbf', gamma=2, C=1, probability=True)),
+          ('Classification Tree', DecisionTreeClassifier(max_depth=5)),
+          ('Random Forest', RandomForestClassifier(max_depth=5, n_estimators=50))]
 
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(my_data, ans, test_size=.4, random_state=42)
 
-clf.fit(X_train, y_train)
-score = clf.score(X_test, y_test)
+for model in models:
+    print("\n\n######################\n", model[0], "\n######################\n")
+    clf = model[1]
+    clf.fit(X_train, y_train)
+    score = clf.score(X_test, y_test)
 
-print(score)
+    print(score)
 
-Ans = clf.predict_proba(X_test)
-count1 = 0
-count2 = 0
-tab = [[] for i in range(70)]
+    Ans = clf.predict_proba(X_test)
+    count1 = 0
+    count2 = 0
+    tab = [[] for i in range(70)]
 
-for i in range(y_test.size):
-    print("Classe : ", classes[y_test[i] - 1], " | probaility : ", Ans[i][y_test[i] - 1], " | rank : ", rank(y_test[i] - 1, Ans[i]))
-    tab[y_test[i] - 1] = rank(y_test[i] - 1, Ans[i])
-    if rank(y_test[i] - 1, Ans[i]) <= 10:
-        count1 += 1
-    if rank(y_test[i] - 1, Ans[i]) <= 2:
-        count2 += 1
-print("# test rank <= 10 ", count1, count1 / y_test.size)
-print("# test rank <= 2 ", count2, count2 / y_test.size)
-print("# test ", y_test.size)
+    for i in range(y_test.size):
+        # print("Classe : ", classes[y_test[i] - 1], " | probaility : ", Ans[i][y_test[i] - 1], " | rank : ", rank(y_test[i] - 1, Ans[i]))
+        tab[y_test[i] - 1].append(rank(y_test[i] - 1, Ans[i]))
+        if rank(y_test[i] - 1, Ans[i]) <= 10:
+            count1 += 1
+        if rank(y_test[i] - 1, Ans[i]) <= 2:
+            count2 += 1
+    print("# test rank <= 10 ", count1, count1 / y_test.size)
+    print("# test rank <= 2 ", count2, count2 / y_test.size)
+    print("# test ", y_test.size)
 
-for i in range(70):
-    print(classes[i], tab[i])
+    for i in range(70):
+        m = 0
+        for x in tab[i]:
+            m += x
+        m /= len(tab[i])
+        if m <= 10:
+            print("[good] ", classes[i], tab[i], m)
+        else:
+            print("[bad]  ", classes[i], tab[i], m)
 #y_result = clf.predict(X_test)
 """
 for i in range(y_test.size):
