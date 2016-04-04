d4ad7c8d50bfb2f1cdc13eaea716122e76443e9f
d1laj <dimitri.lajou@ens-lyon.fr>
2016-04-04 20:44:43 +0200

    Truc sur le script python

--- script/script.py
+++ script/script.py
@@ -2,8 +2,18 @@
 
 import sklearn
 from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
 from numpy import genfromtxt
 import numpy as np
+
+
+def rank(idx, tab):
+    rk = 1
+    for i in tab:
+        if i > tab[idx]:
+            rk += 1
+    return rk
+
 classes = genfromtxt('classes.csv', delimiter=',', dtype=type(""))
 
 ans = genfromtxt('../source/build/answers.csv', delimiter=',', dtype=type(1))
@@ -15,6 +25,7 @@ my_data = genfromtxt('../source/build/train.csv', delimiter=',', dtype=type(1.))
 print(my_data)
 
 clf = SVC(kernel='rbf', gamma=2, C=1, probability=True)
+clf = LogisticRegression()
 
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(my_data, ans, test_size=.4, random_state=42)
@@ -24,7 +35,19 @@ score = clf.score(X_test, y_test)
 
 print(score)
 
-y_result = clf.predict(X_test)
+Ans = clf.predict_proba(X_test)
+count1 = 0
+count2 = 0
+for i in range(y_test.size):
+    print("Classe : ", classes[y_test[i] - 1], " | probaility : ", Ans[i][y_test[i] - 1], " | rank : ", rank(y_test[i] - 1, Ans[i]))
+    if rank(y_test[i] - 1, Ans[i]) <= 10:
+        count1 += 1
+    if rank(y_test[i] - 1, Ans[i]) <= 2:
+        count2 += 1
+print("# test rank <= 10 ", count1, count1 / y_test.size)
+print("# test rank <= 2 ", count2, count2 / y_test.size)
+print("# test ", y_test.size)
+#y_result = clf.predict(X_test)
 """
 for i in range(y_test.size):
     if y_result[i] == y_test[i]:
